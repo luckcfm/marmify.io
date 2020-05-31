e1eb819eaf3c37b95aa67a0db2c8ee140635d537
Lucas Medeiros <lucasmedeiros@viacast.tv>
2020-05-30 21:57:08 -0300

    Fazendo um limpa.

--- src/containers/Restaurante/Pratos/PratosRestaurante.js
+++ src/containers/Restaurante/Pratos/PratosRestaurante.js
@@ -34,19 +34,6 @@ const pratosMaisPedidos = [
   },
 ];
 
-const cars = [
-  { brand: "VW", year: 2012, color: "Orange", vin: "dsad231ff" },
-  { brand: "Audi", year: 2011, color: "Black", vin: "gwregre345" },
-  { brand: "Renault", year: 2005, color: "Gray", vin: "h354htr" },
-  { brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh" },
-  { brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34" },
-  { brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj" },
-  { brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr" },
-  { brand: "Jaguar", year: 2013, color: "Orange", vin: "greg34" },
-  { brand: "Ford", year: 2000, color: "Black", vin: "h54hw5" },
-  { brand: "Fiat", year: 2013, color: "Red", vin: "245t2s" },
-];
-
 const pratos = pratosMaisPedidos.map((prato) => {
   return (
     <div className="p-col-3">
@@ -91,6 +78,7 @@ export const PratosRestaurante = (props) => {
               <Column field="ingredientes" header="Ingredientes" />
               <Column field="disponivel" header="Disponivel" />
               <Column field="totalItem" header="Preco (R$)" />
+              <Column field="disponivel" header="Disponivel" />
             </DataTable>
           </Card>
         </div>
