51ce9bf21a439e20e60ee633066100139756d650
Lucas Medeiros <lucasmedeiros@viacast.tv>
2020-06-07 00:17:18 -0300

    pagina de checkout feita, faltando salvar no banco de dados.

--- src/components/Marmify/Pratos/Prato/CheckoutPrato/CheckoutPrato.js
+++ src/components/Marmify/Pratos/Prato/CheckoutPrato/CheckoutPrato.js
@@ -1,29 +1,88 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Card } from "primereact/card";
 import Modal from '../../../../UI/Modal/Modal';
 import comida_padrao from '../../../../../assets/comida_padrao.jpg'
+import { Button } from 'primereact/button';
 export default function CheckoutPrato(props) {
-    const prato = props.prato;
-    console.log(prato);
-    let img_prato = null;
-    if(!prato || !prato.foto){
-        img_prato = <div className="p-col-12" style={{float: 'center', margin: '0 auto'}}><img style={{height: '200px', float: 'center', margin: '0 auto'}} alt="Foto prato" src={comida_padrao}></img></div>
-    }else{
-        img_prato = <div className="p-col-12"><img alt="Foto prato" src={prato.foto}></img></div>
+  const prato = props.prato;
+  const [checkoutPrato, setCheckoutPrato] = useState({});
+  let img_prato = null;
+  if (!prato || !prato.foto) {
+    img_prato = <div className="p-col-12" style={{ float: 'center', margin: '0 auto' }}><img style={{ height: '200px', float: 'center', margin: '0 auto' }} alt="Foto prato" src={comida_padrao}></img></div>
+  } else {
+    img_prato = <div className="p-col-12"><img alt="Foto prato" src={prato.foto}></img></div>
+  }
+
+  const addItem = (item) => {
+    const newCheckoutPrato = {...checkoutPrato};
+    if(!Object.keys(newCheckoutPrato).length > -1){
+      //inicializamos o prato caso o mesmo nao exista.
+      const newPrato = {...prato}
+      newPrato.itens_escolhidos = [];
+      setCheckoutPrato(newPrato);
     }
-    return (
-        <Modal show={props.showModal} modalClosed={props.modalClosed}>
-            <Card title={prato.nome_prato} subTitle={props.prato.descricao}>
-                {img_prato}
-                {props.prato.descricao}
-                <h3>Itens</h3>
-                <i>Selecione os itens adicionais para adicionar ao seu pedido.</i>
-                <ul>
-                    {prato.itens && prato.itens.map(item => {
-                        return <li>{item.nome_item}  <span style={{float: 'right'}}><b>R$ {item.preco_item}</b></span></li>
-                    })}
-                </ul>
-            </Card>
-        </Modal>
-    )
+    const newPrato = {...checkoutPrato};
+    if(!newPrato.itens_escolhidos){
+      newPrato.itens_escolhidos = [];
+    }
+    newPrato.itens_escolhidos.push(item);
+    setCheckoutPrato(newPrato);
+  }
+  const removeItem = (item) => {
+    console.log('removing item ', item);
+  }
+  const totalItem = () => {
+    
+    let preco_base = parseFloat(prato.preco_base);
+    let total = preco_base
+    if(checkoutPrato.itens_escolhidos){
+      checkoutPrato.itens_escolhidos.map(item => {
+        total += parseFloat(item.preco_item);
+      })
+    }
+    return total.toFixed(2);
+  }
+  return (
+    <Modal show={props.showModal} modalClosed={props.modalClosed}>
+      <Card title={prato.nome_prato} subTitle={props.prato.descricao}>
+        {img_prato}
+        {props.prato.descricao}
+        <h3>Itens</h3>
+        <i>Selecione os itens adicionais para adicionar ao seu pedido.</i>
+        <ul>
+          {prato.itens && prato.itens.map(item => {
+            let escolhidos = [];
+            if(checkoutPrato.itens_escolhidos) {
+              escolhidos = checkoutPrato.itens_escolhidos.filter(myItem => {
+                if(myItem.nome_item === item.nome_item)
+                {
+                  return myItem;
+                }
+              })
+            }
+            return <><li>
+              {item.nome_item} (<b>R$ {item.preco_item}</b>)
+              <span style={{float: 'right'}}>
+                {escolhidos.length}
+                <i onClick={()=> addItem(item)} className="pi pi-plus" style={{cursor: 'pointer'}}></i>
+                <i onClick={()=>removeItem(item)} className="pi pi-minus" style={{cursor: 'pointer'}}></i>
+              </span>
+            </li>
+            <br></br>
+            </>
+          })}
+        </ul>
+        <hr></hr>
+        <span style={{ float: 'right' }}>Total: <b>R$ {totalItem()}</b></span>
+        <br></br>
+        <div style={{ float: 'right' }}>
+          <Button className="p-button-danger" label="Cancelar"></Button>
+
+          <Button label="Adicionar ao carrinho"></Button>
+        </div>
+        <br></br>
+        <br></br>
+      </Card>
+    </Modal>
+  )
 }
